fefbeaccecd7dd917c80ba3dc0a56f6d341c2c0d
Fiskus21$ <pwmurawski123@gmail.com>
2022-12-06 00:26:45 +0100

    add

--- src/hooks/getStarWarsData.ts
+++ src/hooks/getStarWarsData.ts
@@ -7,23 +7,24 @@ import { IVehicles } from "../types/VehiclesTypes";
 const useCreateStarWarsData = (personData: IPeople | undefined) => {
   const { dispatch } = useContext(GlobalContext);
 
-  const addStarWarsDataToGlobalContext = () => {
+  const addStarWarsDataToGlobalContext = async () => {
     if (personData) {
-      personData.vehicles.forEach(async (vehiclesUrl) => {
-        const res = await swApi<IVehicles[]>(
+      const vehiclesPromise = personData.vehicles.map(async (vehiclesUrl) => {
+        const res = await swApi<IVehicles>(
           vehiclesUrl.substring(vehiclesUrl.lastIndexOf("api") + 3)
         );
+        return res?.data;
+      });
 
-        if (res?.data) {
-          dispatch({
-            type: "addStarWarsData",
-            starWarsData: {
-              name: personData.name,
-              created: personData.created,
-              vehicles: res.data,
-            },
-          });
-        }
+      Promise.all(vehiclesPromise).then((vehicles) => {
+        dispatch({
+          type: "addStarWarsData",
+          starWarsData: {
+            name: personData.name,
+            created: personData.created,
+            vehicles: vehicles as IVehicles[],
+          },
+        });
       });
     }
   };
